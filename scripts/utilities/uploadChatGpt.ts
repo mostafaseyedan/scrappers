fc160ad3acdc8fd90a5a52d3e504a1988e2e0966
WT <wtran@cendien.com>
2025-08-07 09:11:54 -0500

    uploadChatGpt now report stats back

--- scripts/utilities/uploadChatGpt.ts
+++ scripts/utilities/uploadChatGpt.ts
@@ -1,16 +1,19 @@
 import "dotenv/config";
-import { solicitation } from "../../app/models";
+import { solicitation, scriptLog as logModel } from "@/app/models";
 import fs from "fs";
 import path from "path";
+import chalk from "chalk";
+import { secToTimeStr } from "@/lib/utils";
 
 const BASE_URL = process.env.BASE_URL || "http://localhost:3000";
 const TOKEN = process.env.SERVICE_KEY;
+const VENDOR = "chatgptweb";
 
 async function uploadBidsyncJson() {
   // Path to the JSON file
   const filePath = path.resolve(
     __dirname,
-    "../../.output/bidsync/2025-08-06.json"
+    `../../.output/${VENDOR}/2025-08-06.json`
   );
   const fileContent = fs.readFileSync(filePath, "utf-8");
   const records = JSON.parse(fileContent);
@@ -26,8 +29,12 @@ async function uploadBidsyncJson() {
       });
 
       if (checkRecord.results?.length) {
-        console.log("  Record already exists ", checkRecord.results[0].id);
+        console.log(
+          chalk.grey("  Record already exists ", checkRecord.results[0].id)
+        );
+        dupCount++;
 
+        /*
         await solicitation.patch({
           id: checkRecord.results[0].id,
           baseUrl: BASE_URL,
@@ -35,6 +42,7 @@ async function uploadBidsyncJson() {
           data: { ...record },
         });
         console.log("  Updated existing record");
+        */
 
         continue; // Skip if the record already exists
       }
@@ -46,11 +54,51 @@ async function uploadBidsyncJson() {
         token: TOKEN,
         data: record,
       });
-      console.log("  Uploaded ", result.id);
+      console.log(chalk.green("  Saved", result.id));
+      successCount++;
     } catch (err) {
-      console.error("  Failed to upload record:", err);
+      console.error(chalk.red("  Failed to upload record:", err));
+      failCount++;
     }
   }
 }
 
-uploadBidsyncJson();
+let dupCount = 0;
+let failCount = 0;
+let successCount = 0;
+let junkCount = 0;
+performance.mark("start");
+
+uploadBidsyncJson()
+  .catch((error: any) => console.error(chalk.red(`  ${error}`, error?.stack)))
+  .finally(async () => {
+    performance.mark("end");
+    const totalSec = Number(
+      (
+        performance.measure("total-duration", "start", "end").duration / 1000
+      ).toFixed(0)
+    );
+
+    console.log(
+      `\nTotal time: ${secToTimeStr(totalSec)} ${new Date().toLocaleString()}`
+    );
+    console.log(
+      `Success: ${successCount}, Failures: ${failCount}, Duplicates: ${dupCount}, Junk: ${junkCount}`
+    );
+
+    await logModel.post({
+      baseUrl: BASE_URL,
+      data: {
+        message: `Scrapped ${successCount} solicitations from ${VENDOR}. 
+            ${failCount > 0 ? `Found ${failCount} failures. ` : ""}
+            ${dupCount > 0 ? `Found ${dupCount} duplicates. ` : ""}`,
+        scriptName: `scrapers/${VENDOR}`,
+        dupCount,
+        successCount,
+        failCount,
+        junkCount,
+        timeStr: secToTimeStr(totalSec),
+      },
+      token: process.env.SERVICE_KEY,
+    });
+  });
