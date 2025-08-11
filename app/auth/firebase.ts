54fe3c34cf21dd0fa97a0eb5f737323df2f6ee55
WT <wtran@cendien.com>
2025-08-11 13:22:53 -0500

    save point with the extra auth stuff

---
+++ app/auth/firebase.ts
@@ -0,0 +1,42 @@
+import { getApp, getApps, initializeApp } from "firebase/app";
+import {
+  connectAuthEmulator,
+  getAuth,
+  inMemoryPersistence,
+  setPersistence,
+} from "firebase/auth";
+import { clientConfig } from "@/config/clientConfig";
+
+export const getFirebaseApp = () => {
+  if (getApps().length) {
+    return getApp();
+  }
+
+  return initializeApp(clientConfig);
+};
+
+export function getFirebaseAuth() {
+  const auth = getAuth(getFirebaseApp());
+
+  // App relies only on server token. We make sure Firebase does not store credentials in the browser.
+  // See: https://github.com/awinogrodzki/next-firebase-auth-edge/issues/143
+  setPersistence(auth, inMemoryPersistence);
+
+  if (process.env.NEXT_PUBLIC_AUTH_EMULATOR_HOST) {
+    // https://stackoverflow.com/questions/73605307/firebase-auth-emulator-fails-intermittently-with-auth-emulator-config-failed
+    (auth as unknown as any)._canInitEmulator = true;
+    connectAuthEmulator(
+      auth,
+      `http://${process.env.NEXT_PUBLIC_AUTH_EMULATOR_HOST}`,
+      {
+        disableWarnings: true,
+      }
+    );
+  }
+
+  if (clientConfig.tenantId) {
+    auth.tenantId = clientConfig.tenantId;
+  }
+
+  return auth;
+}
---
+++ app/register/RegisterPage.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import * as React from "react";
+import { useLoadingCallback } from "react-loading-hook";
+import {
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+} from "firebase/auth";
+import Link from "next/link";
+import { getFirebaseAuth } from "../auth/firebase";
+import { Button } from "@/components/uiAuth/Button";
+import { MainTitle } from "@/components/uiAuth/MainTitle";
+import { PasswordForm } from "@/components/uiAuth/PasswordForm";
+import { PasswordFormValue } from "@/components/uiAuth/PasswordForm/PasswordForm";
+import { LoadingIcon } from "@/components/uiAuth/icons";
+import { appendRedirectParam } from "@/app/shared/redirect";
+import { useRedirectParam } from "@/app/shared/useRedirectParam";
+import styles from "./register.module.css";
+import { useRedirectAfterLogin } from "@/app/shared/useRedirectAfterLogin";
+import { loginWithCredential } from "@/app/api";
+
+export function RegisterPage() {
+  const [hasLogged, setHasLogged] = React.useState(false);
+  const redirect = useRedirectParam();
+  const redirectAfterLogin = useRedirectAfterLogin();
+
+  const [registerWithEmailAndPassword, isRegisterLoading, error] =
+    useLoadingCallback(async ({ email, password }: PasswordFormValue) => {
+      setHasLogged(false);
+      const auth = getFirebaseAuth();
+      const credential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      await loginWithCredential(credential);
+      await sendEmailVerification(credential.user);
+      redirectAfterLogin();
+
+      setHasLogged(true);
+    });
+
+  return (
+    <div className={styles.page}>
+      <MainTitle>Register</MainTitle>
+      {hasLogged && (
+        <div className={styles.info}>
+          <span>
+            Redirecting to <strong>{redirect || "/"}</strong>
+          </span>
+          <LoadingIcon />
+        </div>
+      )}
+      {!hasLogged && (
+        <PasswordForm
+          onSubmit={registerWithEmailAndPassword}
+          loading={isRegisterLoading}
+          error={error}
+        >
+          <Link href={appendRedirectParam("/login", redirect)}>
+            <Button disabled={isRegisterLoading}>Back to login</Button>
+          </Link>
+        </PasswordForm>
+      )}
+    </div>
+  );
+}
